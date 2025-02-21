8fc95c7a9a5460ffd71732f8e6f57e0443de7373
chl0227 <chunlong0227@gmail.com>
2025-02-21 12:00:25 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,12 @@
+///week01-1.cpp 要自己打好全部的檔名，後面要打.cpp
+///在瘋狂程設(練習模式)寫好的程式碼，全選，Ctrl-c
+///CodeBlocks File-New-Empty 空白檔案，Crtl-s存檔
+
+#include <stdio>
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d", a+ b);
+
+}
