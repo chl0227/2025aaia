ca2848ac04ecda62b015d6793ab04e1ebda948ae
龍一 <chunlong0227@gmail.com>
2025-03-21 10:22:32 +0800

    Delete week03-1.py

--- week03-1.py
+++
@@ -1,8 +0,0 @@
-```python
-#week03-1.py
-#LeetCode 2579. Count Total Number of Colored Cells
-class Solution:
-    def coloredCells(self, n: int) -> int:
-        return n*n + (n-1)*(n-1)
-        #觀察題目的圖片(不看英文、不看中文)，投轉45度，看到正方形
-```
